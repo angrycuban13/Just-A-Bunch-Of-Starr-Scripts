4ba36d91ba158d54afbd66d86832f5feec5fab3a
Angry Cuban <39564898+angrycuban13@users.noreply.github.com>
2022-10-23 21:57:39 -0500

    More WIP

--- Upgradinatorr/upgradinatorr.ps1
+++ Upgradinatorr/upgradinatorr.ps1
@@ -218,6 +218,31 @@ function Get-TagId {
 
 }
 
+function Get-QualityProfile {
+    [CmdletBinding()]
+    param (
+        $app,
+        [string[]]$profileName,
+        $url
+    )
+
+    if ($app -like '*Radarr*') {
+        $apiversion = $api_version_radarr
+    }
+    elseif ($app -like '*Sonarr*') {
+        $apiversion = $api_version_sonarr
+    }
+
+    $qualityProfiles = Invoke-RestMethod -Uri "$($url)/api/$($apiversion)/qualityprofile" -Headers $webHeaders -Method Get -StatusCodeVariable apiStatusCode
+
+    if (Confirm-ArrResponse($apiStatusCode)) {
+        $qualityProfilesIds = $qualityProfiles | Where-Object { $_.name -in $profileName } | Select-Object -ExpandProperty id
+        $qualityProfilesIds
+    }
+    else {
+        throw 'Failed to get quality profiles'
+    }
+}
 function Read-IniFile {
     [CmdletBinding()]
     param (
@@ -426,6 +451,12 @@ foreach ($app in $apps) {
     $webHeaders = @{
         'x-api-key' = $appConfig.'ApiKey'
     }
+    if ($($appConfig.'ProfileName') -ne '') {
+        $profileName = $($appConfig.'ProfileName') -split ','
+    }
+    if ($($appConfig.'TagIgnore') -ne '') {
+        $tagIgnore = $($appConfig.'TagIgnore') -split ','
+    }
 
     if ($logVerbose) {
         Write-Output 'Verbose logging enabled'
@@ -445,10 +476,37 @@ foreach ($app in $apps) {
 
     Write-Output "Starting $($appTitle) search"
     Write-Output "Config - URL: $($appUrl)"
-    Write-Output "Config - Tag name: $($tagName)"
     Write-Output "Config - Monitored: $($appMonitored)"
     Write-Output "Config - Unattended: $($runUnattended)"
     Write-Output "Config - $((Get-Culture).TextInfo.ToTitleCase($app)) Limit: $($count)"
+    switch ($profileName) {
+        $null {
+            Write-Warning 'Config - Profile: No profile specified'
+        }
+        { $profileName.count -eq 1 } {
+            Write-Output "Config - Profile: $profileName"
+        }
+    }
+    if ($profileName.count -gt 1) {
+        Write-Output "Config - Profile: $($profileName -join ', ')"
+    }
+    Write-Output "Config - Tag name: $($tagName)"
+    switch ($tagIgnore) {
+        $null {
+            Write-Warning 'Config - Tag ignore list: No tag blacklist specified'
+        }
+        { $tagIgnore.count -eq 1 } {
+            Write-Output "Config - Tag ignore list: $tagIgnore"
+        }
+    }
+    if ($tagIgnore.count -gt 1) {
+        Write-Output "Config - Tag ignore list: $($tagIgnore -join ', ')"
+    }
+
+    if ($null -ne $profileName) {
+        $qualityProfileId = Get-QualityProfile -App $app -Url $appUrl -ProfileName $profileName
+        Write-Output "Quality Profile IDs $($qualityProfileId -join ', ') confirmed for $appTitle"
+    }
 
     if ($logVerbose) {
         Write-Output 'Verbose logging enabled'
@@ -460,12 +518,25 @@ foreach ($app in $apps) {
 
     if ($app -like '*radarr*') {
         $allMovies = Get-ArrItems -app $app -url $appUrl
+        $allMovies | Select-Object -First 1
 
-        Write-Verbose "Retrieved a total of $($allMovies.Count) movies"
+        Write-Output "Retrieved a total of $($allmovies.Count) movies"
 
-        $filteredMovies = $allMovies | Where-Object { $_.tags -notcontains $tagId -and $_.monitored -eq $appMonitored -and $_.status -eq $appConfig.'MovieStatus' }
+        if ($null -ne $profileName ) {
+            $filteredMovies = $allMovies | Where-Object { $_.tags -notcontains $tagId -and $_.monitored -eq $appMonitored -and $_.status -eq $appconfig.'MovieStatus' -and $_.qualityProfileId -notin $qualityProfileId }
+        }
+        elseif ($null -ne $tagIgnore) {
+            //TODO
+        }
+        elseif (($null -ne $tagIgnore) -and ($null -ne $profileName)) {
+            //TODO
 
-        Write-Verbose "Filtered movies down to $($filteredMovies.count) movies"
+        }
+        elseif (($null -eq $profileName) -and ($null -eq $tagIgnore)) {
+            $filteredMovies = $allMovies | Where-Object { $_.tags -notcontains $tagId -and $_.monitored -eq $appMonitored -and $_.status -eq $appconfig.'MovieStatus' }
+        }
+
+        Write-Output "Filtered movies down to $($filteredMovies.count) movies"
 
         if ($filteredMovies.Count -eq 0 -and $runUnattended -ne $false) {
             $moviesWithTag = $allMovies | Where-Object { $_.tags -contains $tagId -and $_.monitored -eq $appMonitored -and $_.status -eq $appConfig.'MovieStatus' }
@@ -543,7 +614,7 @@ foreach ($app in $apps) {
                 Add-Tag -App $app -Movies $randomMovies -TagId $tagID -Url $appUrl
             }
 
-            foreach ($movie in $randomMovies) {
+            <#foreach ($movie in $randomMovies) {
                 $movieCounter++
                 Write-Progress -Activity 'Search in Progress' -PercentComplete (($movieCounter / $count) * 100)
 
@@ -557,7 +628,7 @@ foreach ($app in $apps) {
                     }
                     Write-Output "Manual search kicked off for $($movie.title)"
                 }
-            }
+            }#>
         }
     }
 
